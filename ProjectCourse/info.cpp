55fbb353f9413513d157bbe92f4eb7b40683cb06
dns <dynuli01@mail.com>
2024-12-26 19:49:36 +0700

    Создание формы info

---
+++ ProjectCourse/info.cpp
@@ -0,0 +1,2 @@
+﻿#include "info.h"
+
--- ProjectCourse/login.cpp
+++ ProjectCourse/login.cpp
@@ -1,2 +1,14 @@
-#include "login.h"
+﻿#include "login.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Project::login form;
+	Application::Run(% form);
+	return 0;
+}
