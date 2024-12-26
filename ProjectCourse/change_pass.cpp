f69193c61dae79272cecbe4a3eba6ae453d5683b
dns <dynuli01@mail.com>
2024-12-26 19:59:15 +0700

    Добавление формы change_pass

---
+++ ProjectCourse/change_pass.cpp
@@ -0,0 +1,2 @@
+﻿#include "change_pass.h"
+
---
+++ ProjectCourse/change_pass.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+
+namespace ProjectCourse {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для change_pass
+	/// </summary>
+	public ref class change_pass : public System::Windows::Forms::Form
+	{
+	public:
+		change_pass(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~change_pass()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = gcnew System::ComponentModel::Container();
+			this->Size = System::Drawing::Size(300,300);
+			this->Text = L"change_pass";
+			this->Padding = System::Windows::Forms::Padding(0);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		}
+#pragma endregion
+	};
+}
